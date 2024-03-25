7585e78a0baa57c97f1b6aab3557ded4f33ba41b
VYSHAK HARIKUMAR <vyshakharikumar98@gmail.com>
2024-03-25 16:47:14 +0530

    attraction order list done

--- src/app/(Dashboard)/profile/order/ActractionOrders.tsx
+++ src/app/(Dashboard)/profile/order/ActractionOrders.tsx
@@ -1,33 +1,117 @@
+"use client";
+import ComponentLoader from "@/components/ComponentLoader";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+interface Order {
+  _id: string;
+  referenceNumber: string;
+  totalAmount: number;
+  updatedAt: number;
+  orderStatus: string;
+  attraction: {
+    logo: string;
+    title: string;
+    images: string[];
+  };
+  activities: {
+    adultsCount: number;
+    childrenCount: number;
+  };
+}
 
 export default function ActractionOrders() {
+  const [orders, setOrders] = useState([]);
+  const { user, jwtToken } = useSelector((state: any) => state.user);
+  const [loading, setLoading] = useState(true);
+
+  const formatDate = (date: number) => {
+    const formattedDate = new Date(date).toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+    return formattedDate;
+  };
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        if (!jwtToken) {
+          throw new Error("JWT token is missing");
+        }
+
+        const response = await fetch(
+          "https://api-server-i1.mytravellerschoice.com/api/v1/attractions/orders/all?limit=50",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`Failed to fetch orders: ${response?.statusText}`);
+        }
+
+        const data = await response?.json();
+        const orderData = data?.result;
+        setOrders(orderData.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchOrders();
+  }, [jwtToken]);
+
+  if (loading) {
+    return (
+      <div className="space-y-2 p-5 mt-5">
+        <ComponentLoader />
+        <ComponentLoader />
+        <ComponentLoader />
+        <ComponentLoader />
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="text-2xl sm:text-2xl md:text-3xl font-medium my-10 ">
         Attraction Orders
       </h1>
-      <Link href={'/profile/order/order-details'} className="w-full flex flex-col sm:flex-row gap-3  h-fit border rounded-xl overflow-hidden p-2 ">
-        <img
-          className="w-full sm:w-[250px] h-[200px]  sm:h-[150px]  rounded-xl flex-shrink-0"
-          src="https://mytravellerschoice.com/_next/image?url=https%3A%2F%2Fapi-server-i1.mytravellerschoice.com%2Fpublic%2Fimages%2Fhome%2Fimages-1672464923312-853360323.jpg%20&w=828&q=75"
-          alt="img"
-        />
-        <section className="w-full text-sm flex flex-col h-[200px]  sm:h-[150px] justify-between">
-          <div className="flex justify-between">
-          <h1>Ref No: B2CATO_1710933715074814</h1>
-          <h1>Wed Mar 20 2024</h1>
-          </div>
-          <h1>Name: DUBAI PARKS & RESORTS</h1>
-          <h1>Adult Count:1</h1>
-          <h1>Child Count:1</h1>
-          <div  className="flex justify-between">
-          <h1>Status: pending</h1>
-          <h1>Total Amount: 295.00 AED</h1>
-          </div>
-        </section>
-      </Link>
-      
+      {orders?.map((order: Order, index) => (
+        <div key={index} className="mt-2">
+          <Link
+            href={`/profile/order/${order?._id}`}
+            className="w-full flex flex-col sm:flex-row gap-3  h-fit border rounded-xl overflow-hidden p-2 "
+          >
+            <img
+              className="w-full sm:w-[250px] h-[200px]  sm:h-[150px]  rounded-xl flex-shrink-0"
+              src={`https://api-server-i1.mytravellerschoice.com/${order?.attraction?.images[0]}`}
+              alt="img"
+            />
+            <section className="w-full text-sm flex flex-col h-[200px]  sm:h-[150px] justify-between">
+              <div className="flex justify-between">
+                <h1>Ref No: {order?.referenceNumber}</h1>
+                <h1>{formatDate(order?.updatedAt)}</h1>
+              </div>
+              <h1>Name: {order?.attraction?.title}</h1>
+              <h1>Adult Count:{order?.activities?.adultsCount}</h1>
+              <h1>Child Count:{order?.activities?.childrenCount}</h1>
+              <div className="flex justify-between">
+                <h1>Status: {order?.orderStatus}</h1>
+                <h1>Total Amount: {order?.totalAmount} AED</h1>
+              </div>
+            </section>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
